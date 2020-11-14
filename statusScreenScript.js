d8383cdc19a8e7f9655c10e12ab8d589738c50ec
Dylan Jung <dj5822@gmail.com>
2020-11-14 21:40:49 +1300

    Status screen basic functionality working.

--- statusScreenScript.js
+++ statusScreenScript.js
@@ -1,6 +1,40 @@
+// calculate current time.
+function updateTime() {
+  var today = new Date();
+  var currentDate = today.getDate() + "/" + (today.getMonth()+1) + "/" + today.getFullYear();
+  var currentTime = "";
+  if (today.getHours() > 12) {
+    currentTime = formatTime(today.getHours()-12) + ":" +
+    formatTime(today.getMinutes()) + ":" + formatTime(today.getSeconds()) + " PM";
+  }
+  else {
+    currentTime = formatTime(today.getHours()) + ":" +
+    formatTime(today.getMinutes()) + ":" + formatTime(today.getSeconds()) + " AM";
+  }
 
+  var millisecExisted = (today - birthDate);
+  var daysExisted = Math.floor(millisecExisted/1000/60/60/24);
+  var hoursExisted = Math.floor(millisecExisted/1000/60/60)%24;
+  var minsExisted = Math.floor(millisecExisted/1000/60)%60;
+  var secsExisted = Math.floor(millisecExisted/1000)%60;
+
+
+  document.getElementById("current-date").innerHTML = currentDate;
+  document.getElementById("current-time").innerHTML = currentTime;
+  document.getElementById("play-time").innerHTML = daysExisted + " days " +
+  hoursExisted + " hours " + minsExisted + " mins " + secsExisted + " secs";
+  setTimeout(updateTime, 500);
+}
+
+function formatTime(value){
+  if (value<10){
+    value = "0" + value;
+  }
+  return value;
+}
+
+const birthDate = new Date('June 17, 2000 00:00:00');
 document.getElementById("name").innerHTML = "Dylan Jung";
 document.getElementById("age").innerHTML = "20";
 document.getElementById("birth-date").innerHTML = "17/06/2000";
-document.getElementById("current-date").innerHTML = "???";
-document.getElementById("play-time").innerHTML = "???";
+updateTime();
