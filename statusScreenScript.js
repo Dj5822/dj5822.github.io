6e10f2b3cfb4ce6033b26a4849fe8447fb8371df
Dylan Jung <dj5822@gmail.com>
2020-11-14 21:01:45 +1300

    Added javascript to change variables
    The variables in the html document now
    depend on the javascript document.

---
+++ statusScreenScript.js
@@ -0,0 +1,6 @@
+
+document.getElementById("name").innerHTML = "Dylan Jung";
+document.getElementById("age").innerHTML = "20";
+document.getElementById("birth-date").innerHTML = "17/06/2000";
+document.getElementById("current-date").innerHTML = "???";
+document.getElementById("play-time").innerHTML = "???";
