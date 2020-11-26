2e607877cd6ad8b8f7110932048f5e4546ad21d4
Dylan Jung <dj5822@gmail.com>
2020-11-26 15:49:26 +1300

    Replaced getElementById with JQuery selectors
    Organised the code into functions.
    All variables are now at the top of the script.
    Replaced getElementById with JQuery selector.

--- statusScreenScript.js
+++ statusScreenScript.js
@@ -1,31 +1,58 @@
+// global variables
+const name = "Dylan Jung";
+const birthDate = new Date('June 17, 2000 00:00:00');
+var today = new Date();
+
 // calculate current time.
 function updateTime() {
-  var today = new Date();
-  var currentDate = today.getDate() + "/" + (today.getMonth()+1) + "/" + today.getFullYear();
-  var currentTime = "";
+  // Update values.
+  today = new Date();
+  $("#age").html(today.getFullYear()-birthDate.getFullYear());
+  $("#current-date").html(dateToString(today));
+  $("#current-time").html(getCurrentTime());
+  $("#play-time").html(getPlayTime());
+
+  setTimeout(updateTime, 500);
+}
+
+/*
+Returns the current date.
+*/
+function dateToString(date) {
+  return formatTime(date.getDate()) + "/" + formatTime(date.getMonth()+1) +
+  "/" + date.getFullYear();
+}
+
+/*
+Gets the current time in the form: hrs:min:sec:AM/PM
+*/
+function getCurrentTime(){
   if (today.getHours() < 12) {
-    currentTime = formatTime(((today.getHours()+11)%12)+1) + ":" +
+    return formatTime(((today.getHours()+11)%12)+1) + ":" +
     formatTime(today.getMinutes()) + ":" + formatTime(today.getSeconds()) + " AM";
   }
   else {
-    currentTime = formatTime(((today.getHours()-1)%12)+1) + ":" +
+    return formatTime(((today.getHours()-1)%12)+1) + ":" +
     formatTime(today.getMinutes()) + ":" + formatTime(today.getSeconds()) + " PM";
   }
+}
 
+/*
+Calculates total play time.
+*/
+function getPlayTime(){
   var millisecExisted = (today - birthDate);
   var daysExisted = Math.floor(millisecExisted/1000/60/60/24);
   var hoursExisted = Math.floor(millisecExisted/1000/60/60)%24;
   var minsExisted = Math.floor(millisecExisted/1000/60)%60;
   var secsExisted = Math.floor(millisecExisted/1000)%60;
-
-
-  document.getElementById("current-date").innerHTML = currentDate;
-  document.getElementById("current-time").innerHTML = currentTime;
-  document.getElementById("play-time").innerHTML = daysExisted + " days " +
-  hoursExisted + " hours " + minsExisted + " mins " + secsExisted + " secs";
-  setTimeout(updateTime, 500);
+  return daysExisted + " days " + hoursExisted + " hours " +
+  minsExisted + " mins " + secsExisted + " secs";
 }
 
+/* Adds zeros to the front of time values such that there
+are two digits in the time value.
+*/
 function formatTime(value){
   if (value<10){
     value = "0" + value;
@@ -33,8 +60,8 @@ function formatTime(value){
   return value;
 }
 
-const birthDate = new Date('June 17, 2000 00:00:00');
-document.getElementById("name").innerHTML = "Dylan Jung";
-document.getElementById("age").innerHTML = "20";
-document.getElementById("birth-date").innerHTML = "17/06/2000";
+// These values do not change.
+$("#name").html(name);
+$("#birth-date").html(dateToString(birthDate));
+// Update values that do change.
 updateTime();
